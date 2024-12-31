90c22b6dafaa8ebad8bb70a015a640fc1fe53af2
Maick Tonet <maicktonet4@gmail.com>
2024-12-31 17:33:34 -0300

    added helmet to change the titles of the movie view page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { HelmetProvider } from "react-helmet-async";
 import { AppSidebar } from "./components/app-sidebar";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
@@ -7,20 +8,22 @@ import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SidebarProvider>
-        <AppSidebar />
-        <main className="w-full">
-          <header className="flex items-center p-2  font-bold text-lg lg:px-4 shadow ">
-            <div className="flex items-center w-full space-x-3">
-              <SidebarTrigger className="lg:hidden hover:text-white" />
-              <h2>Movie's Catalogue Project</h2>
-            </div>
-            <ModeToggle />
-          </header>
-          {children}
-        </main>
-      </SidebarProvider>
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="w-full">
+            <header className="flex items-center p-2  font-bold text-lg lg:px-4 shadow ">
+              <div className="flex items-center w-full space-x-3">
+                <SidebarTrigger className="lg:hidden hover:text-white" />
+                <h2>Movie's Catalogue Project</h2>
+              </div>
+              <ModeToggle />
+            </header>
+            {children}
+          </main>
+        </SidebarProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
--- src/pages/movie-view.tsx
+++ src/pages/movie-view.tsx
@@ -2,6 +2,7 @@ import BreadcrumbPath from "@/components/breadcrumb-path";
 import Loading from "@/components/loading";
 import MovieOverview from "@/components/movie-overview";
 import { useMovieQueries } from "@/hooks/use-movie-query";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 
 export default function MovieView() {
@@ -12,18 +13,13 @@ export default function MovieView() {
     return <Loading />;
   }
 
-  // TODO: Add breadcrumb in app layout
-
   return (
-    <main className="px-6 py-4">
-      <nav className="w-full p-4 mb-3">
-        {movie && <BreadcrumbPath movie={movie} />}
-      </nav>
-      {movie && (
-        <section>
-          <MovieOverview movie={movie} />
-        </section>
-      )}
+    <main>
+      <Helmet>
+        <title>{movie?.title} - Movie Catalogue</title>
+      </Helmet>
+      {movie && <BreadcrumbPath movie={movie} />}
+      {movie && <MovieOverview movie={movie} />}
     </main>
   );
 }
