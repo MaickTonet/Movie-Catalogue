d8caa5a502fd69d6c6a59bb90fe73b9f047dd4ff
Maick Tonet <maicktonet4@gmail.com>
2025-02-02 20:44:51 -0300

    chore: added the names of the companies to their listing on the Movie View page.

--- src/components/productionCompaniesView.tsx
+++ src/components/productionCompaniesView.tsx
@@ -10,14 +10,11 @@ export default function ProductionCompaniesView({ movie }: { movie: Movie }) {
           <h2 className='text-center text-xl font-bold'>Produção</h2>
           <div className='flex w-full flex-wrap items-center justify-center gap-4'>
             {movie.production_companies.map((company) => (
-              <div key={company.id} className='max-w-24'>
+              <div key={company.id} className='max-w-24 transition-all duration-300 ease-in-out hover:scale-95'>
                 {company.logo_path && (
-                  <LazyLoadImage
-                    src={`https://image.tmdb.org/t/p/original/${company.logo_path}`}
-                    alt={company.name}
-                    className='transition-all duration-300 ease-in-out hover:scale-95'
-                  />
+                  <LazyLoadImage src={`https://image.tmdb.org/t/p/original/${company.logo_path}`} alt={company.name} />
                 )}
+                <p className='mt-2 text-center text-sm text-muted'>{company.name} </p>
               </div>
             ))}
           </div>
