ecc5fd6512e9a04de87301794f865997b3a7aa49
Maick Tonet <maicktonet4@gmail.com>
2025-02-02 22:27:45 -0300

    chore: make the title a shortcut to the homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 import { AppSidebar } from './components/app-sidebar'
 import { ModeToggle } from './components/mode-toggle'
 import { ThemeProvider } from './components/theme-provider'
@@ -16,7 +17,9 @@ export default function App({ children }: { children: React.ReactNode }) {
             <header className='sticky top-0 z-50 flex items-center bg-background p-2 text-lg font-bold shadow lg:px-4'>
               <div className='flex w-full items-center space-x-3'>
                 <SidebarTrigger className='hover:text-white lg:hidden' />
-                <h2>Movie's Catalogue</h2>
+                <Link to={'/'} className='w-full'>
+                  Movie's Catalogue
+                </Link>
               </div>
               <ModeToggle />
             </header>
