6f2c62edd10d0bc00e1c7840655219576bcc8f7e
Maick Tonet <maicktonet4@gmail.com>
2025-02-09 19:04:42 -0300

    fix: ajust movie view page width

--- src/components/movie-overview.tsx
+++ src/components/movie-overview.tsx
@@ -1,17 +1,16 @@
 import { formatDateByYear, formatMovieRunTime } from '@/helpers/dataFormat'
 import { Movie } from '@/types/movieTypes'
 import CastCarousel from './castCarousel'
-import CollectionView from './collectionView'
 import GenderBadgesList from './genderBadgesList'
+import MovieCarousel from './movieCarousel'
 import MovieGeneralInfo from './movieGeneralInfo'
 import MovieImagesDialog from './movieImagesDialog'
-import ProductionCompaniesView from './productionCompaniesView'
-import MovieCarousel from './movieCarousel'
 
 export default function MovieOverview({ movie }: { movie: Movie }) {
   return (
-    <section className={`relative flex flex-col flex-wrap items-center justify-center space-y-3 md:flex-row md:items-start pb-12`}>
-      <aside className='flex flex-col items-center space-y-3 pb-3'>
+    <section
+      className={`relative flex flex-col flex-wrap items-center justify-center space-y-3 pb-12 md:flex-row md:items-start`}>
+      <aside className='flex max-w-sm flex-col items-center space-y-3 pb-3'>
         <MovieImagesDialog movie={movie} />
         <div className='flex w-[80%] flex-col items-center justify-center space-y-2'>
           <h2 className='text-center text-2xl font-bold'>{movie.title}</h2>
@@ -24,10 +23,8 @@ export default function MovieOverview({ movie }: { movie: Movie }) {
       <aside className='flex w-[80%] max-w-lg flex-col items-start gap-6 pb-8'>
         <MovieGeneralInfo movie={movie} />
         <CastCarousel movie={movie} />
-        <CollectionView movie={movie} />
-        <ProductionCompaniesView movie={movie} />
       </aside>
-      <MovieCarousel items={movie.recommendations.results} title={'Recomendados'} mediaType={'movie'}  />
+      <MovieCarousel items={movie.recommendations.results} title={'Recomendados'} mediaType={'movie'} />
     </section>
   )
 }
